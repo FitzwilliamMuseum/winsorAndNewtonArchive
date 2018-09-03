136e0141212514454fea17d7e3b2cc1fa72bb50e
Daniel Pett <dpett@britishmuseum.org>
2018-09-03 12:05:35 +0100

    Remove tags

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,4 +1,3 @@
-<script>
 (function(f,i,t,z,w,n,a){f['GoogleAnalyticsObject']=w;f[w]=f[w]||function(){
         (f[w].q=f[w].q||[]).push(arguments)},f[w].l=1*new Date();n=i.createElement(t),
     a=i.getElementsByTagName(t)[0];n.async=1;n.src=z;a.parentNode.insertBefore(n,a)
@@ -6,4 +5,3 @@
 
 ga('create', 'UA-3319190-10', 'auto');
 ga('send', 'pageview');
-</script>
